2779926b55316dcc090fb8cac738f1f223b1f6df
piratehams <83624673+piratehams@users.noreply.github.com>
2022-12-28 16:33:33 -0300

    v1.0

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,8 +5,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
-public class MainManager : MonoBehaviour
-{
+public class MainManager : MonoBehaviour {
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
@@ -15,24 +14,21 @@ public class MainManager : MonoBehaviour
     public GameObject BestScoreText;
 
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < LineCount; ++i) {
+            for (int x = 0; x < perLine; ++x) {
                 Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
                 var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
                 brick.PointValue = pointCountArray[i];
@@ -40,8 +36,7 @@ public class MainManager : MonoBehaviour
             }
         }
 
-        if (GameManager.Instance.scoreColection.PlayerName.Count > 0)
-        {
+        if (GameManager.Instance.scoreColection.PlayerName.Count > 0) {
             string playerName = GameManager.Instance.scoreColection.PlayerName[GameManager.Instance.scoreColection.bestScoreIndex];
             int bestScore = GameManager.Instance.scoreColection.Scores[GameManager.Instance.scoreColection.bestScoreIndex];
             BestScoreText.GetComponent<TextMeshProUGUI>().text = $"Best Score : {playerName} : {bestScore}";
@@ -49,12 +44,9 @@ public class MainManager : MonoBehaviour
 
     }
 
-    private void Update()
-    {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
+    private void Update() {
+        if (!m_Started) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
                 m_Started = true;
                 float randomDirection = Random.Range(-1.0f, 1.0f);
                 Vector3 forceDir = new Vector3(randomDirection, 1, 0);
@@ -63,24 +55,19 @@ public class MainManager : MonoBehaviour
                 Ball.transform.SetParent(null);
                 Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
             }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
+        } else if (m_GameOver) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
     }
 
-    void AddPoint(int point)
-    {
+    void AddPoint(int point) {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
     }
 
-    public void GameOver()
-    {
+    public void GameOver() {
         m_GameOver = true;
         GameOverText.SetActive(true);
 
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -5,36 +5,45 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class MenuUIHandler : MonoBehaviour
-{
+public class MenuUIHandler : MonoBehaviour {
+
+    public GameObject BestScoreText;
+
+    public void Start() {
+        string playerName = GameManager.Instance.scoreColection.PlayerName[GameManager.Instance.scoreColection.bestScoreIndex];
+        int bestScore = GameManager.Instance.scoreColection.Scores[GameManager.Instance.scoreColection.bestScoreIndex];
+        BestScoreText.GetComponent<TextMeshProUGUI>().text = $"Best Score : {playerName} : {bestScore}";
+    }
+
+
+
     [SerializeField] private GameObject inputField;
 
-    public void TextChange()
-    {
+    public void TextChange() {
         GameManager.Instance.PlayerName = inputField.GetComponent<TMP_InputField>().text;
     }
 
-    public void StartNew()
-    {
-        if (GameManager.Instance.PlayerName == "" || GameManager.Instance.PlayerName == null)
-        {
+    public void StartNew() {
+        if (GameManager.Instance.PlayerName == "" || GameManager.Instance.PlayerName == null) {
             GameManager.Instance.PlayerName = "Default";
         }
         SceneManager.LoadScene(1);
     }
 
-    public void Scores()
-    {
+    public void Scores() {
         SceneManager.LoadScene(2);
     }
 
-    public void Quit()
-    {
-    #if UNITY_EDITOR
+    public void Settings() {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Quit() {
+#if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
-    #else
+#else
         Application.Quit();
-    #endif
+#endif
     }
 
     public void Back() {
